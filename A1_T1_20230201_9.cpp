0bc51f7defa79d0091e65406b17d0c08ad7e8008
Abdelrahman <alibody1010@gmail.com>
2024-10-10 01:12:55 +0300

    added problem number 9

--- A1_T1_20230201_9.cpp
+++ A1_T1_20230201_9.cpp
@@ -2,38 +2,43 @@
 #define nl '\n'
 using namespace std;
 
-
+//Function to check bears count can reach 42 using recurrsion
 bool bears(int k){
-    if(k == 42){
-        return true;
-    }
-    if (k < 42){
-        return false;
-    }
-
-
+    //Case cases
+        if(k == 42){
+            return true;
+        }
+        if (k < 42){
+            return false;
+        }
+        
+    //Check if the number mod 5 equalls 0
     if(k % 5 == 0){
         if(bears (k- 42)){
             return true;
         }
     }
-
+    //Check if number is mod 4 or 3 equalls 0
     if(k % 4 == 0 || k % 3 == 0){
-        int l_digit = k % 10;
-        int l2_digit = (k % 100) / 10;
+
+        int l_digit = k % 10; //Getting last digit
+        int l2_digit = (k % 100) / 10;//Getting second last digit
         int value_to_return = l_digit * l2_digit;
+       //Condition to check if the multiplying condtion doesn't equal 0
         if(value_to_return > 0){
             if(bears((k - value_to_return))){
                 return true;
             }
         }
     }
-
+    //Check if number is even
     if(k % 2 == 0){
         if(bears (k / 2)){
             return true;
         }
     }
+
+    //basic return value
     return false;
 }
 
